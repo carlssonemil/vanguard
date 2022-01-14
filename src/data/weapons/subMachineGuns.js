048f4b29a67569fb4e09e0eb199159a54642c467
Lucas Floriani <lucas.floriani@outlook.com>
2022-01-14 19:24:35 -0300

    Added season 1 reloaded missing weapon

--- src/data/weapons/subMachineGuns.js
+++ src/data/weapons/subMachineGuns.js
@@ -1,6 +1,6 @@
 import { defaultProgress } from '../defaults'
 
-const weapons = ['MP-40', 'Sten', 'M1928', 'Owen Gun', 'Type 100', 'PPSh-41']
+const weapons = ['MP-40', 'Sten', 'M1928', 'Owen Gun', 'Type 100', 'PPSh-41', 'Welgun']
 const original = ['MP-40', 'Sten', 'M1928', 'Owen Gun', 'Type 100', 'PPSh-41']
 
 export default weapons.map(weapon => ({
