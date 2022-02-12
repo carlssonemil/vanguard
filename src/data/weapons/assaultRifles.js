ece5ed7da3e0632a965a1c7c2f626e1371a8d8b2
Billy Curtis <38408756+BillyCurtis@users.noreply.github.com>
2022-02-12 20:48:26 +0000

    Added Season 2 Weapons

--- src/data/weapons/assaultRifles.js
+++ src/data/weapons/assaultRifles.js
@@ -1,6 +1,6 @@
 import { defaultProgress } from '../defaults'
 
-const weapons = ['STG44', 'Automaton', 'Itra Burst', 'Bar', 'AS44', 'NZ-41', 'Volkssturmgewehr', 'Cooper Carbine']
+const weapons = ['STG44', 'Automaton', 'Itra Burst', 'Bar', 'AS44', 'NZ-41', 'Volkssturmgewehr', 'Cooper Carbine', 'KG M40']
 const original = ['STG44', 'Automaton', 'Itra Burst', 'Bar', 'AS44', 'NZ-41', 'Volkssturmgewehr']
 
 export default weapons.map(weapon => ({
