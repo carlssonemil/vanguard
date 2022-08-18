4957095b4add56168cd56cbb18f917be219cb359
CrazyIsEasy <crazyiseasyy@gmail.com>
2022-08-18 13:16:09 -0400

    Season 5 Weapons

--- src/data/weapons/handguns.js
+++ src/data/weapons/handguns.js
@@ -1,6 +1,6 @@
 import { defaultProgress } from '../defaults'
 
-const weapons = ['Ratt', 'Top Break', '1911', 'Klauser', 'Machine Pistol']
+const weapons = ['Ratt', 'Top Break', '1911', 'Klauser', 'Machine Pistol', 'Valois Revolver']
 const original = ['Ratt', 'Top Break', '1911', 'Klauser', 'Machine Pistol']
 
 export default weapons.map(weapon => ({
